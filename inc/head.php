5151b4f2e4522a2b2639c58395ecaf66af26da54
allecosta <alle_costa10@outlook.com>
2023-03-24 16:45:17 -0300

    chore: release v0.3

--- inc/head.php
+++ inc/head.php
@@ -3,14 +3,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta name="description" content="Somos a mudança, conectamos você ao futuro" />
+    <meta name="keywords" content="software, sistemas, developer, desenvolvimento, sites, websites, e-commerce" />
+    <meta property="og:title" content="AC Sistemas & Internet">
+    <meta property="og:description" content="Somos a mudança, conectamos você ao futuro">
+    <meta property="og:image" content="">
     
+    <title>AC Sistemas & Internet</title>
+
+    <link href="./img/icon_logo.png" rel="icon">
     <link href="css/bootstrap.min.css" rel="stylesheet" />
     <link href="css/fancybox/jquery.fancybox.css" rel="stylesheet"> 
     <link href="css/flexslider.css" rel="stylesheet" /> 
     <link href="css/magnific-popup.css" rel="stylesheet">
     <link href="css/gallery-1.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet" />
-    <title>AC Sistemas & Internet</title>
 </head>
